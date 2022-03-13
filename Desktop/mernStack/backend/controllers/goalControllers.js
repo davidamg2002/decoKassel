354b8a4d85d95c4eafc073ad1ec02d9161e55c8e
davidamg2002 <davidamg2002@gmail.com>
2022-03-13 07:12:16 +0100

    Initial REST API for goals

--- Desktop/mernStack/backend/controllers/goalControllers.js
+++ Desktop/mernStack/backend/controllers/goalControllers.js
@@ -4,8 +4,8 @@ const Goal = require("../models/goalModel");
 // @method GET
 // @access Private
 const getGoals = asyncHandler(async (req, res) => {
-  const goal = await Goal.find();
-  res.status(200).json(goal);
+  const goals = await Goal.find();
+  res.status(200).json(goals);
 });
 // @desc Get goals
 // @method POST
@@ -23,15 +23,31 @@ const setGoal = asyncHandler(async (req, res) => {
 // @method PUT
 // @access Private
 const updateGoal = asyncHandler(async (req, res) => {
-  const goal = await Goal.findByIdAndUpdate(req.params.id);
+  const goal = await Goal.findById(req.params.id);
 
-  res.status(200).json({ text: `${goal}` });
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedGoal);
 });
 // @desc Get goals
 // @method DELETE
 // @access Private
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ text: "Delete goals" });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  // await goal.remove();
+  await Goal.remove(goal);
+  res.status(200).json({ id: req.params.id, message: "Goal removed" });
 });
 
 module.exports = {
